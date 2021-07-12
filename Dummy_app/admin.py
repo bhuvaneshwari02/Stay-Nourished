45d3d9ad8905c7a3282ff2c487211055ccb5ebae
BANUPRIYA K <76400807+BANUPRIYA10-1@users.noreply.github.com>
2021-07-12 08:36:51 +0530

    Staynourished Application

---
+++ Dummy_app/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Diet
+from .models import Food
+from .models import Contact
+
+admin.site.register(Diet)
+admin.site.register(Food)
+admin.site.register(Contact)
+
+
+
+# Register your models here.
